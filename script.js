504a8bc7e9497f29c9e36cd4b31e0b43117177d1
FeelEtc <filippoff.a.n@gmail.com>
2020-05-22 21:43:01 +0300

    add onenter click clear input value

--- script.js
+++ script.js
@@ -14,4 +14,29 @@ btn.addEventListener("click", function() {
     let taskValue = document.createElement("label");
     task.append(taskValue);
     taskValue.innerHTML = str;
+
+    document.querySelector(".newTask").value = ""
 });
+
+
+document.querySelector('input').addEventListener('keydown', function(e) {
+   if (e.keyCode === 13) {
+
+    let str = document.querySelector(".newTask").value;//значение из поля ввода   
+    let list = document.querySelector(".list");
+
+    let task = document.createElement("div");
+    task.className = "task";
+    list.append(task);
+
+    let done = document.createElement("input");
+    done.type = "checkbox";
+    task.append(done);
+
+    let taskValue = document.createElement("label");
+    task.append(taskValue);
+    taskValue.innerHTML = str;
+   
+    document.querySelector(".newTask").value = ""
+   }
+    });
