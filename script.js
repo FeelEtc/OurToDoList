be5b5cba553390dc7001c36fc072d6b1de5f72f0
FeelEtc <filippoff.a.n@gmail.com>
2020-05-25 22:37:02 +0300

    add DeleteButton to task refactoring: extract function feel
    Co-authored-by: TanyaShved <tankonatanke90@gmail.com>

--- script.js
+++ script.js
@@ -1,42 +1,35 @@
-let btn = document.querySelector("button");
-btn.addEventListener("click", function() {
-    let str = document.querySelector(".newTask").value;//значение из поля ввода   
-    let list = document.querySelector(".list");
+const feel = function () {
+  let str = document.querySelector(".newTask").value; //значение из поля ввода
+  let list = document.querySelector(".list");
 
-    let task = document.createElement("div");
-    task.className = "task";
-    list.append(task);
+  let task = document.createElement("div");
+  task.className = "task";
+  list.append(task);
 
-    let done = document.createElement("input");
-    done.type = "checkbox";
-    task.append(done);
+  let done = document.createElement("input");
+  done.type = "checkbox";
+  task.append(done);
 
-    let taskValue = document.createElement("label");
-    task.append(taskValue);
-    taskValue.innerHTML = str;
+  let taskValue = document.createElement("label");
+  task.append(taskValue);
+  taskValue.innerHTML = str;
 
-    document.querySelector(".newTask").value = ""
+  let deleteButton = document.createElement("button");
+  deleteButton.className = "delBut";
+  deleteButton.innerHTML = "X";
+  task.append(deleteButton);
+  deleteButton.addEventListener('click', function(){
+    task.remove()
+  })
+
+  document.querySelector(".newTask").value = "";
+};
+
+document.querySelector("button").addEventListener("click", feel);
+
+document.querySelector("input").addEventListener("keydown", function (e) {
+  if (e.keyCode === 13) {
+    feel();
+  }
 });
 
-
-document.querySelector('input').addEventListener('keydown', function(e) {
-   if (e.keyCode === 13) {
-
-    let str = document.querySelector(".newTask").value;//значение из поля ввода   
-    let list = document.querySelector(".list");
-
-    let task = document.createElement("div");
-    task.className = "task";
-    list.append(task);
-
-    let done = document.createElement("input");
-    done.type = "checkbox";
-    task.append(done);
-
-    let taskValue = document.createElement("label");
-    task.append(taskValue);
-    taskValue.innerHTML = str;
-   
-    document.querySelector(".newTask").value = ""
-   }
-    });
